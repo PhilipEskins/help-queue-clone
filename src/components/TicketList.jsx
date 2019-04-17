ee2126b8d27d7b67294aade065754973079ff672
Chris Breaux and Philip Eskins <philip@>
2019-04-17 09:52:55 -0700

    display project when you type npm run start

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Ticket from './Ticket';
+
+function TicketList() {
+  return(
+      <Ticket
+        location="4F"
+        names="Chris and Philip"
+        issue="Computer is on fire!!"/>
+  );
+}
+
+export default TicketList;
