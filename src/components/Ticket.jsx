8f824f5e677872ab15a5b7d8a7e4e430515e3c6f
Chris Breaux and Philip Eskins <chris@>
2019-04-17 10:27:12 -0700

    add stylejsx to project

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types';
 function Ticket(props) {
   return (
     <div>
+      <style global jsx>{`
+        div {
+          background-color: red;
+        }
+            `}
+      </style>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr />
@@ -18,24 +24,3 @@ Ticket.propTypes = {
 };
 
 export default Ticket;
-//
-// import React from "react";
-// import PropTypes from "prop-types";
-//
-// function Ticket(props){
-//   return (
-//       <div>
-//         <h3>{props.location} - {props.names}</h3>
-//         <p><em>{props.issue}</em></p>
-//         <hr/>
-//       </div>
-//   );
-// }
-//
-// Ticket.propTypes = {
-//   names: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   issue: PropTypes.string
-// };
-//
-// export default Ticket;
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+const masterTicketList = [
+  {
+    names: 'Chris and Philip',
+    location: '3A',
+    issue: 'Computer is on fire!'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Fox image not displaying on page, can only see duck?'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Donkey picture not displaying on hover in Zoology app. :('
+  }
+];
+
 function TicketList() {
   return(
-    <Ticket
-      location="4F"
-      names="Chris and Philip"
-      issue="Computer is on fire!!"/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index} />
+      )}
+    </div>
   );
 }
 
